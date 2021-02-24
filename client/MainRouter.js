9f9dba790d08ae7927a989b4a29c90a74e38755d
bythealphabet <bythealphabet@gmail.com>
2021-02-23 20:10:20 -0400

    chaning text rendered to page

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,13 +1,6 @@
 import React from "react";
 
 function MainRouter() {
-  return (
-    <div>
-      this, it is Hey IM GO hjhjhj Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Corrupti error maiores ullam similique suscipit. Quidem
-      ex cum modi beatae maiores deleniti dicta mollitia nobis autem molestiae,
-      a eveniet, provident praesentium?
-    </div>
-  );
+	return <div>Hi from React</div>;
 }
 export default MainRouter;
